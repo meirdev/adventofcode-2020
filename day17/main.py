4d1e992eead8907bdc856d2fe38acc564394f4fc
Meir <maornet@gmail.com>
2024-03-01 15:58:39 +0200

    refactor

--- day17/main.py
+++ day17/main.py
@@ -11,40 +11,41 @@ def parse_input(input: str) -> list[list[State]]:
     return [list(map(State, line)) for line in input.strip().splitlines()]
 
 
-def part1(input: str) -> int:
+def solution(input: str, dimensions: int):
     cubes_init = parse_input(input)
 
-    neighbors = list(itertools.product([-1, 0, 1], repeat=3))
-    neighbors.remove((0, 0, 0))
-
     cubes = {
-        (y, x, 0)
+        tuple([y, x] + [0] * (dimensions - 2))
         for y in range(len(cubes_init))
         for x in range(len(cubes_init[y]))
         if cubes_init[y][x] == State.ACTIVE
     }
 
+    neighbors = list(itertools.product([-1, 0, 1], repeat=dimensions))
+    neighbors.remove(tuple([0] * dimensions))
+
     for _ in range(6):
-        limits = [
+        limits = (
             range(
                 min(cubes, key=lambda p: p[i])[i] - 1,
                 max(cubes, key=lambda p: p[i])[i] + 2,
             )
-            for i in range(3)
-        ]
+            for i in range(dimensions)
+        )
 
         temp_cubes = set()
 
-        for y, x, z in itertools.product(*limits):
-            cube = (y, x, z)
-
+        for cube in itertools.product(*limits):
             active = 0
 
-            for ny, nx, nz in neighbors:
-                n_cube = (ny + y, nx + x, nz + z)
+            for neighbor in neighbors:
+                n_cube = tuple(cube[i] + neighbor[i] for i in range(dimensions))
 
                 if n_cube in cubes:
                     active += 1
+
+                if active > 3:
+                    break
 
             if (cube in cubes and active in [2, 3]) or (
                 cube not in cubes and active == 3
@@ -56,49 +57,12 @@ def part1(input: str) -> int:
     return len(cubes)
 
 
+def part1(input: str) -> int:
+    return solution(input, 3)
+
+
 def part2(input: str) -> int:
-    cubes_init = parse_input(input)
-
-    neighbors = list(itertools.product([-1, 0, 1], repeat=4))
-    neighbors.remove((0, 0, 0, 0))
-
-    cubes = {
-        (y, x, 0, 0)
-        for y in range(len(cubes_init))
-        for x in range(len(cubes_init[y]))
-        if cubes_init[y][x] == State.ACTIVE
-    }
-
-    for _ in range(6):
-        limits = [
-            range(
-                min(cubes, key=lambda p: p[i])[i] - 1,
-                max(cubes, key=lambda p: p[i])[i] + 2,
-            )
-            for i in range(4)
-        ]
-
-        temp_cubes = set()
-
-        for y, x, z, w in itertools.product(*limits):
-            cube = (y, x, z, w)
-
-            active = 0
-
-            for ny, nx, nz, nw in neighbors:
-                n_cube = (ny + y, nx + x, nz + z, nw + w)
-
-                if n_cube in cubes:
-                    active += 1
-
-            if (cube in cubes and active in [2, 3]) or (
-                cube not in cubes and active == 3
-            ):
-                temp_cubes.add(cube)
-
-        cubes = temp_cubes
-
-    return len(temp_cubes)
+    return solution(input, 4)
 
 
 def main() -> None:
